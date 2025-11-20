41e6a9be593b143e5597c54e788cfbae015c11c2
fourbarman <maks.java@yandex.ru>
2025-11-21 00:29:58 +0400

    Refactored function - returns List. Added size conditions. Added tests.

--- src/main/kotlin/ru/job4j/base/DrawX.kt
+++ src/main/kotlin/ru/job4j/base/DrawX.kt
@@ -3,18 +3,23 @@ package ru.job4j.base
 private const val ELEVEN = 11
 
 /* Draw X. size > 0 и нечетное */
-fun draw(size: Int) {
+fun draw(size: Int): List<List<Char>> {
+    if ( size < 0 || size % 2 == 0) return emptyList()
+
     val start = 0
-    for (value in start until size) {
-        for (value2 in start until size) {
-            if (value == value2 || value + value2 == size - 1) {
-                print("X")
+    val xList = ArrayList<List<Char>>()
+    for (row in start until size) {
+        val xCellList = ArrayList<Char>()
+        for (cell in start until size) {
+            if (row == cell || row + cell == size - 1) {
+                xCellList.add('X')
             } else {
-                print(" ")
+                xCellList.add(' ')
             }
         }
-        println()
+        xList.add(xCellList)
     }
+    return xList
 }
 
 fun main() {
