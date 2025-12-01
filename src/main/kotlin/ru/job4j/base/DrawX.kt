3c12f44a698fc2092806cccbf6335175874e56ad
fourbarman <maks.java@yandex.ru>
2025-12-01 12:56:48 +0400

    Refactored logic.

--- src/main/kotlin/ru/job4j/base/DrawX.kt
+++ src/main/kotlin/ru/job4j/base/DrawX.kt
@@ -2,24 +2,29 @@ package ru.job4j.base
 
 /* Draw X. size > 0 и нечетное */
 fun draw(size: Int): List<List<Char>> {
-    if ( size < 0 || size % 2 == 0) return emptyList()
+    if (size < 0 || size % 2 ==0) {
+        return emptyList()
+    }
 
-    val start = 0
-    val rows = ArrayList<List<Char>>()
-    for (row in start until size) {
-        val rowCells = ArrayList<Char>()
-        for (cell in start until size) {
-            if (row == cell || row + cell == size - 1) {
-                rowCells.add('X')
-            } else {
-                rowCells.add(' ')
-            }
+    val rows = ArrayList<ArrayList<Char>>()
+    var index = 0
+
+    while (index < size * size) {
+        val row = index / size
+        val cell = index % size
+
+        if (rows.size <= row) {
+            rows.add(ArrayList())
         }
-        rows.add(rowCells)
+
+        val rowCells = rows[row]
+
+        if (row == cell || row + cell == size -1) {
+            rowCells.add('X')
+        } else {
+            rowCells.add(' ')
+        }
+        index++
     }
     return rows
-}
-
-fun main() {
-    draw(11)
 }
