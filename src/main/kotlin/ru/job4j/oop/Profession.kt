be86ed14134dced4dacbf66c4ff87b37e1012c5b
fourbarman <maks.java@yandex.ru>
2026-03-24 18:55:07 +0400

    override methods

--- src/main/kotlin/ru/job4j/oop/Profession.kt
+++ src/main/kotlin/ru/job4j/oop/Profession.kt
@@ -3,19 +3,38 @@ package ru.job4j.oop
 open class Profession(
     var name: String,
     var experience: Int
-)
+) {
+    open fun action() {
+        println("$name with $experience years of experience makes action")
+    }
+}
 
 class ITManager(
     name: String,
     experience: Int
-) : Profession(name, experience)
+) : Profession(name, experience) {
+
+    override fun action() {
+        println("$name with $experience years of experience makes IT")
+    }
+}
 
 class Lawyer(
     name: String,
     experience: Int
-) : Profession(name, experience)
+) : Profession(name, experience) {
+
+    override fun action() {
+        println("$name with $experience years of experience read laws")
+    }
+}
 
 class Teacher(
     name: String,
     experience: Int
-) : Profession(name, experience)
+) : Profession(name, experience) {
+
+    override fun action() {
+        println("$name with $experience years of experience is teaching")
+    }
+}
