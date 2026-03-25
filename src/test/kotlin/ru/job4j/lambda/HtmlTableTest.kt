7f64bc7a4f475f347031c5b4b6494983ee2bd30d
fourbarman <maks.java@yandex.ru>
2026-03-25 21:11:10 +0400

    Apply

---
+++ src/test/kotlin/ru/job4j/lambda/HtmlTableTest.kt
@@ -0,0 +1,19 @@
+package ru.job4j.lambda
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class HtmlTableTest {
+    @Test
+    fun whenTableThenShouldStartWithTable() {
+        val htmlTable = HtmlTable()
+        assertThat(htmlTable.table(5, 5)).startsWith("<table>")
+    }
+
+    @Test
+    fun whenTableThenShouldEndWithTable() {
+        val htmlTable = HtmlTable()
+        assertThat(htmlTable.table(5, 5)).endsWith("<table>")
+    }
+}
